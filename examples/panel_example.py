8de1e2e8604e13c8a7685f45970c65f0643d42c0
BirukBelihu <birukbelihu1995@gmail.com>
2025-08-29 08:10:50 +0300

    Adds examples for new views

--- examples/panel_example.py
+++ examples/panel_example.py
@@ -1,10 +1,15 @@
 from pyglow.styles.border import Border
 from pyglow.views.panel import Panel
 
-panel = Panel("pyglow",
+def main():
+ panel = Panel("pyglow",
            "Console output formatter library",
          "#09ED1C",
       "#E9ED09",
       "#ED093F",
-              Border.ROUNDED)
-panel.show()
+              Border.ROUNDED
+             )
+ panel.show()
+
+if __name__ == "__main__":
+    main()
--- examples/spinner_example.py
+++ examples/spinner_example.py
@@ -2,14 +2,21 @@ import time
 import threading
 from pyglow.views.spinner import Spinner
 
-spinner = Spinner(prefix="Loading please wait", bar_color="#FFFFFFF", delay=0.1)
+def main():
+ spinner = Spinner(prefix="Loading please wait",
+                   bar_color="#FFFFFFF",
+                   delay=0.1
+                   )
 
-t = threading.Thread(target=spinner.start)
-t.start()
+ t = threading.Thread(target=spinner.start)
+ t.start()
 
-# Simulate some work
-for i in range(1, 3):
+ # Simulate some work
+ for i in range(1, 3):
     time.sleep(1)
 
-# Stop the spinner when done
-spinner.stop()
+ # Stop the spinner when done
+ spinner.stop()
+
+if __name__ == "__main__":
+    main()
