2ca548a674e90324c010526b584348dd8079bf9a
BirukBelihu <birukbelihu1995@gmail.com>
2025-08-29 08:07:05 +0300

    Adds examples for new views

---
+++ examples/panel_example.py
@@ -0,0 +1,10 @@
+from pyglow.styles.border import Border
+from pyglow.views.panel import Panel
+
+panel = Panel("pyglow",
+           "Console output formatter library",
+         "#09ED1C",
+      "#E9ED09",
+      "#ED093F",
+              Border.ROUNDED)
+panel.show()
