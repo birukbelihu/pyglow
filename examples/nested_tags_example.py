6bb96504aecd0da58f07b0917cce4d5806a35577
BirukBelihu <birukbelihu1995@gmail.com>
2025-09-20 12:13:05 +0300

    v1.5.0

--- examples/nested_tags_example.py
+++ examples/nested_tags_example.py
@@ -1,8 +1,32 @@
 from pyglow.pyglow import Glow
+from pyglow.mapping import ANSI_RESET
+from pyglow.styles.background import Back
+from pyglow.styles.foreground import Fore
+from pyglow.styles.style import Style
+
 
 def main():
- Glow.print(
-    "[cyan bold][link=https://github.com/birukbelihu/pyglow]pyglow[/][/][/] is a lightweight, [bold]markdown-style console output formatter[/] library for Python. \nIt enables developers to output styled subtitle in the terminal using simple and readable tags like `[red bold]Error[/]`.")
+    Glow.prints("Python Glow Library Demo", "bold underline magenta")
+    Glow.print("[bg-black Cyan]This is a test of the pyglow library.[/]")
+
+    Glow.print("[rgb(255,56,0) italic underline]Red subtitle[/]")
+    Glow.print("[hex(#00FF00) bold dim]Green subtitle[/]")
+
+    Glow.print("[yellow bold strike]Task completed[/]")
+
+    print(f"{Fore.BRIGHT_YELLOW}{Back.CYAN}{Style.ITALIC}pyglow{ANSI_RESET}")
+
+    Glow.print("[yellow bold blink]Hey there! Do you like pyglow?[/]")
+
+    Glow.print("[bg-blue][fg(rgb(255,255,0)) bold underline]Nested styles with colors[/][/]")
+
+    Glow.print("[link=https://github.com/birukbelihu/pyglow][fg(rgb(0,255,255)) bold]Click me![/][/link]")
+
+    Glow.print(
+        "[fg(rgb(255,0,0))]Red[/] "
+        "[fg(rgb(0,255,0))]Green[/] "
+        "[fg(rgb(0,0,255))]Blue[/]"
+    )
 
 if __name__ == "__main__":
     main()
